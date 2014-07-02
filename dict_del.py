24aeff5525e0bfb433a68c43ca5207eca14274cd
Emerson <viejoemer@gmail.com>
2014-07-02 09:05:33 -0500

    #Howto delete a key from a dict in python?

---
+++ dict_del.py
@@ -0,0 +1,25 @@
+'''
+Created on Jul 1, 2014
+
+@author: viejoemer
+'''
+############################################################
+#Howto delete a key from a dict in python?
+
+#Creating a dict with data
+
+d = {
+     "red":100,
+     "yellow":200,
+     "blue" : 300
+     }
+
+print(d)
+
+#Delete a key
+ 
+del d["red"]
+
+print(d)
+
+print(d)
